eab331dc90fbc5239bb0aa78b902c312fc6dcce9
Philipp Potisk <phil@walt.id>
2021-11-30 16:31:27 +0100

    using waltid.org backend instead of localhost

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,8 +54,8 @@ export default {
   },
 
   proxy: {
-    '/issuer-api/': 'http://localhost:8080',
-    '/api/': 'http://localhost:8080'
+    '/issuer-api/': 'https://wallet.waltid.org',
+    '/api/': 'https://wallet.waltid.org'
   },
 
   auth: {
