561c4b9ae5cb77c605f26b4b95bfef7dca01e45a
Severin Stampler <severin@walt.id>
2021-12-14 13:01:23 +0100

    allow unauthorized access to lux page link to index

--- pages/services-publics-lu.vue
+++ pages/services-publics-lu.vue
@@ -1,3 +1,11 @@
 <template>
+  <nuxt-link to="/">
   <img class="mb-4" src="../assets/services-publics-lu.png" >
+  </nuxt-link>
 </template>
+<script>
+export default {
+  name: "services-publics-lu",
+  auth: false
+}
+</script>
