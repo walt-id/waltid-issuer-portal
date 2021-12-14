366ff4db8e55b46977ca0953d4cd4b23400dbd47
Philipp Potisk <phil@walt.id>
2021-12-14 20:57:30 +0100

    Added login-screen for mobile

--- pages/services-publics-lu-mobile.vue
+++ pages/services-publics-lu-mobile.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="text-center">
   <nuxt-link to="/">
-  <img class="mb-4" src="../assets/services-publics-lu.png" >
+  <img class="mb-4" src="../assets/services-publics-lug-mobile.png" >
   </nuxt-link>
   </section>
 </template>
 <script>
 export default {
-  name: "services-publics-lu",
+  name: "services-publics-lu-mobile",
   auth: false
 }
 </script>
